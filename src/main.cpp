4ff6df0b13550a2a11132e4227611922e686620f
Ahmet Hekim <ahmet1hekim@gmail.com>
2026-01-26 03:33:41 +0300

    static cast valuse from int to float

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,24 @@
 #include <SFML/Graphics.hpp>
+#include <boost/asio.hpp>
+#include <cmath>
 #include <iostream>
 #include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
-#include <cmath>
-#include <boost/asio.hpp>
 
-cv::Mat sfml2opencv(const sf::Image &img)
+cv::Mat sfml2opencv(const sf::Image& img)
 {
     // get size from image
     cv::Size size(img.getSize().x, img.getSize().y);
     // create a mat on image memory
-    cv::Mat mat(size, CV_8UC4, (void *)img.getPixelsPtr(), cv::Mat::AUTO_STEP);
+    cv::Mat mat(size, CV_8UC4, (void*)img.getPixelsPtr(), cv::Mat::AUTO_STEP);
     // make SFML RGBA to OpenCV BGRA
     cv::cvtColor(mat, mat, cv::COLOR_BGR2RGB);
     return mat.clone();
 }
-
-void sendMat(boost::asio::ip::tcp::socket &socket, const cv::Mat &mat)
-{
-    // Meta info
+void sendMat(boost::asio::ip::tcp::socket& socket, const cv::Mat& mat)
+{ // Meta info
     int32_t rows = mat.rows;
     int32_t cols = mat.cols;
     int32_t type = mat.type();
@@ -38,7 +36,7 @@ int main()
 
     boost::asio::io_context io_context;
     boost::asio::ip::tcp::acceptor acceptor(io_context,
-    boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 12345));
+        boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 12345));
 
     std::cout << "Waiting for client on port 12345..." << std::endl;
 
@@ -56,13 +54,13 @@ int main()
 
     // square
     float rect_h = 30.f, rect_w = 20.f;
-    sf::RectangleShape shape({rect_w, rect_h});
+    sf::RectangleShape shape({ rect_w, rect_h });
     shape.setFillColor(sf::Color::Red);
 
     // centering its control point
-    shape.setOrigin({rect_w / 2, rect_h / 2});
+    shape.setOrigin({ rect_w / 2, rect_h / 2 });
     // centering it on the window
-    shape.setPosition({window_res_x / 2, window_res_y / 2});
+    shape.setPosition({ static_cast<float>(window_res_x) / 2, static_cast<float>(window_res_y) / 2 });
 
     // getting the image and creating the texture and the sprites
     sf::FileInputStream bg_stream(std::string(ASSETS_DIR) + "/dag.jpg");
@@ -70,7 +68,7 @@ int main()
     sf::Sprite bg_sprite(bg_texture);
 
     // creating the window and setting vsync so my pc doesnt die
-    sf::RenderWindow window(sf::VideoMode({window_res_x, window_res_y}), "!", sf::Style::None, sf::State::Windowed);
+    sf::RenderWindow window(sf::VideoMode({ window_res_x, window_res_y }), "!", sf::Style::None, sf::State::Windowed);
     window.setVerticalSyncEnabled(true); // call it once after creating the window
     // window.setFramerateLimit(30);
     // setting the sprite's starting point to texture's 0,0 and its size to the window size
@@ -84,49 +82,41 @@ int main()
     auto offset_x = 2.0f, offset_y = 2.0f;
 
     // second window for debug
-    sf::RenderWindow window2(sf::VideoMode({drone_cam_res_x, drone_cam_res_y}), "SFML works!", sf::Style::None, sf::State::Windowed);
+    sf::RenderWindow window2(sf::VideoMode({ drone_cam_res_x, drone_cam_res_y }), "SFML works!", sf::Style::None, sf::State::Windowed);
     window2.setVerticalSyncEnabled(true); // call it once after creating the window
 
     sf::Vector2f view_pos(shape.getPosition().x, shape.getPosition().y);
-    while (window.isOpen())
-    {
+    while (window.isOpen()) {
         // bg_sprite.setTextureRect(sf::IntRect({shape.getPosition().x, shape.getPosition().y}, {window_res_x, window_res_y}));
 
         // std::cout << std::string(ASSETS_DIR) + "/dag.jpg";
 
         // HANDLE KEYBOARD EVENTS
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
                 window.close();
             }
         }
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
-        {
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)) {
             // std::cout << "move left";
             // tomove_x = -speed_x;
             shape.rotate(sf::degrees(-0.2f));
         }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
-        {
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right)) {
             // std::cout << "move right ";
             // tomove_x = speed_x;
             shape.rotate(sf::degrees(0.2f));
         }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down))
-        {
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down)) {
             // std::cout << "move left";
             tomove_y = speed_y;
         }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))
-        {
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up)) {
             // std::cout << "move right ";
             tomove_y = -speed_y;
         }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space))
-        {
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space)) {
             // std::cout << "move right ";
             speed_y += 2.5f;
             speed_x += 2.5f;
@@ -154,9 +144,7 @@ int main()
         float minY = halfShapeSize.y;
         float maxY = texSize2.y - halfShapeSize.y;
 
-        if (futurePos.x >= minX && futurePos.x <= maxX &&
-            futurePos.y >= minY && futurePos.y <= maxY)
-        {
+        if (futurePos.x >= minX && futurePos.x <= maxX && futurePos.y >= minY && futurePos.y <= maxY) {
             shape.move(moveVec);
         }
 
@@ -194,7 +182,7 @@ int main()
             view_pos.y = texSize.y - halfViewH;
 
         view1.setCenter(view_pos);
-        view1.setSize({view_res_x, view_res_y});
+        view1.setSize({ static_cast<float>(view_res_x), static_cast<float>(window_res_y) });
         window.setView(view1);
         window.draw(bg_sprite);
         window.draw(shape);
@@ -203,7 +191,7 @@ int main()
 
         sf::View view;
         view.setCenter(shape.getPosition());
-        view.setSize({drone_cam_res_x, drone_cam_res_y});
+        view.setSize({ static_cast<float>(drone_cam_res_x), static_cast<float>(drone_cam_res_y) });
         view.setRotation(shape.getRotation());
         window2.setView(view);
         window2.clear();
@@ -212,13 +200,13 @@ int main()
         // window2.display();
 
         // get the image
-        sf::Texture windowTexture({window2.getSize().x, window2.getSize().y});
+        sf::Texture windowTexture({ window2.getSize().x, window2.getSize().y });
         windowTexture.update(window2);
         sf::Image croppedImage = windowTexture.copyToImage();
         // cv::imshow("drone_view", sfml2opencv(croppedImage));
         // cv::waitKey(1);
         sendMat(socket, sfml2opencv(croppedImage));
-        float angle = shape.getRotation().asDegrees() ;
+        float angle = shape.getRotation().asDegrees();
         boost::asio::write(socket, boost::asio::buffer(&angle, sizeof(angle)));
     }
 }
